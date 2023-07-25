1484b8837e926f80341fdf03a103e311f1a150f2
GitAfrica <pmogale88@gmail.com>
2023-07-25 21:46:05 +0200

    commit change

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,20 +3,16 @@ import random
 
 number = random.randint(-10000, 10000)
 
-import random
-
-number = random.randint(-10000, 10000)
-
 def last_digit_message(num):
-    last_digit = num % 10
-    output = f"Last digit of {abs(num)} is {abs(last_digit)} and is"
+    last_digit = abs(num) % 10
+    output = f"Last digit of {num} is {last_digit} and is"
 
     if num == 0:
         output += " 0"
     elif last_digit == 0:
         output += " 0"
     elif num < 0:
-        output += " less than 6 and not 0"
+        output += f" less than 6 and not 0"
     elif last_digit > 5:
         output += " greater than 5"
     else:
@@ -25,4 +21,5 @@ def last_digit_message(num):
     return output
 
 print(last_digit_message(number))
+
 
