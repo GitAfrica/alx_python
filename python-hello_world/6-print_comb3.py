a006a8f6d7cc2d9ab0768e0f137f99fa36e28b9f
GitAfrica <pmogale88@gmail.com>
2023-07-25 20:56:54 +0200

    another try

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+result = ""
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
+        result += "{:02d}".format(tens_digit * 10 + ones_digit) + ", "
+
+print(result[:-2])  
+
 
         
 
