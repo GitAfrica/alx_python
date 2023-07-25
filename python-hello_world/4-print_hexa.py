f139f1a268865be1b4709013568a2bdefd663aac
GitAfrica <pmogale88@gmail.com>
2023-07-25 21:04:18 +0200

    fixed error

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
+for num in range(99):
+    print("{:02d} 0x{:02x}".format(num, num))
 
-last_digit = abs(number) % 10
-
-result = f"The string Last digit of {number} is {last_digit} "
-
-if last_digit > 5:
-    result += "and is greater than 5"
-elif last_digit == 0:
-    result += "and is 0"
-else:
-    result += "and is less than 6 and not 0"
-
-print(result)
