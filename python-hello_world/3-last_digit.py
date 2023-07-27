e200a7fab4edf31f0fd6dd359e7db92278e00776
GitAfrica <pmogale88@gmail.com>
2023-07-27 20:58:01 +0200

    fix error

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,23 +4,13 @@ import random
 number = random.randint(-10000, 10000)
 
 def last_digit_message(num):
-    last_digit = abs(num) % 10
-    output = f"Last digit of {num} is {num%10} and is"
-
-    if num == 0:
-        output += " 0"
-    elif last_digit == 0:
-        output += " 0"
+    if num > 0:
+        output = f"{num} is positive."
     elif num < 0:
-        output += f" less than 6 and not 0"
-    elif last_digit > 5:
-        output += " greater than 5"
+        output = f"{num} is negative."
     else:
-        output += " less than 6 and not 0"
+        output = f"{num} is zero."
 
     return output
 
 print(last_digit_message(number))
-
-
-
